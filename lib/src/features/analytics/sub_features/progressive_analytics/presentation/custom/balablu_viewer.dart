96fa487b38956785c262f688edd839f9332c2224
folaoluwfemi <folaoluwafemi55@gmail.com>
2024-01-30 13:00:00 +0100

    implement logic for balancing out weeks in months

--- lib/src/features/analytics/sub_features/progressive_analytics/presentation/custom/balablu_viewer.dart
+++ lib/src/features/analytics/sub_features/progressive_analytics/presentation/custom/balablu_viewer.dart
@@ -12,6 +12,10 @@ class CalendarViewer extends StatefulWidget {
   State<CalendarViewer> createState() => _CalendarViewerState();
 }
 
+typedef Week = List<DateTime>;
+typedef Month = List<Week>;
+typedef Year = List<Month>;
+
 typedef WeeklySnapshots = Map<int, List<ProductivitySnapshot>>;
 typedef MonthlySnapshots = Map<int, WeeklySnapshots>;
 typedef YearlySnapshots = Map<int, MonthlySnapshots>;
@@ -106,27 +110,140 @@ class _CalendarViewerState extends State<CalendarViewer> {
     final List<int> monthsInYear = [
       for (int i = firstMonthInYear; i <= lastMonth; i++) i
     ];
-    final List<MapEntry<int, WeeklySnapshots>> monthsSnapshots = [
-      for (final month in monthsInYear)
-        getWeeksPartForMonth(
-          month: month,
-          year: year,
-          firstDayInMonth: firstSnapshotInFirstMonth.dateTime.day,
-          lastDayInMonth:
-              month == lastMonth ? lastSnapshotInLastMonth.dateTime.day : null,
-        )
-    ];
+    final List<MapEntry<int, WeeklySnapshots>> monthsSnapshots = [];
+
+    for (final month in monthsInYear) {
+      final MapEntry<int, WeeklySnapshots> item = getWeeksPartForMonth(
+        month: month,
+        year: year,
+        firstDayInMonth: firstSnapshotInFirstMonth.dateTime.day,
+        lastDayInMonth:
+            month == lastMonth ? lastSnapshotInLastMonth.dateTime.day : null,
+      );
+      monthsSnapshots.add(item);
+    }
 
     return Map<int, WeeklySnapshots>.fromEntries(monthsSnapshots);
   }
 
-  ProductivitySnapshot balablu(DateTime day) {
+  ProductivitySnapshot snapshotFromDay(DateTime day) {
     try {
       return allSnapshots[day]!;
     } catch (e) {
       print('error day: $day');
       rethrow;
     }
+  }
+
+  MonthlySnapshots balancedOutWeeksInYear(int year) {
+    final firstSnapshotInFirstMonth = snapshots.where((element) {
+      return element.dateTime.year == year;
+    }).first;
+
+    final int firstMonthInYear = firstSnapshotInFirstMonth.dateTime.month;
+
+    final lastSnapshotInLastMonth = snapshots.where((element) {
+      return element.dateTime.year == year;
+    }).last;
+
+    final int lastMonth = lastSnapshotInLastMonth.dateTime.month;
+
+    final List<int> monthsInYear = [
+      for (int i = firstMonthInYear; i <= lastMonth; i++) i
+    ];
+
+    Year year_ = getWeeksForMonth(year: year);
+    year_ = balanceIncompleteWeeks(year_);
+
+    return monthlySnapshotsFromYear(year_);
+  }
+
+  /// composes the [ProductivitySnapshot] value for each day in a [Week]
+  /// in a [Month] in a [Year].
+  MonthlySnapshots monthlySnapshotsFromYear(Year year) {
+    final MonthlySnapshots monthlySnapshots = {};
+
+    for (int i = 0; i < year.length; i++) {
+      final WeeklySnapshots weeklySnapshots = {};
+      final Month month = year[i];
+      for (int i = 0; i < month.length; i++) {
+        final ProductivitySnapshots snapshots = [];
+
+        for (final DateTime day in month[i]) {
+          snapshots.add(snapshotFromDay(day));
+        }
+
+        weeklySnapshots[i] = snapshots;
+      }
+      monthlySnapshots[i] = weeklySnapshots;
+    }
+    return monthlySnapshots;
+  }
+
+  /// If the first week in a [month] is not up to 7 days long,
+  /// then the last week of the [previousMonth] is merged with the first of the [month].
+  /// such that the first week of the [month] is up to 7 days long.
+  /// and the last week of the [previousMonth] is up to 7 days long.
+  Year balanceIncompleteWeeks(Year year) {
+    year = year.copy;
+
+    for (int i = 0; i < year.length; i++) {
+      final Month month = year[i];
+      if (month.first.length == 7) continue;
+      if (i <= 0) continue;
+      final Month previousMonth = year[i - 1];
+      final Week lastWeekOfPreviousMonth = previousMonth.last;
+      final Week firstWeekOfCurrentMonth = month.first;
+      final Week freshMergedWeek = [
+        ...lastWeekOfPreviousMonth,
+        ...firstWeekOfCurrentMonth,
+      ];
+      final Month newMonth = [
+        freshMergedWeek,
+        ...month.sublist(1),
+      ];
+
+      // final Month oldMonth = previousMonth.copy..removeLast();
+
+      year.insert(i, newMonth);
+      year[i - 1].removeLast();
+    }
+
+    return year;
+  }
+
+  Year getWeeksForMonth({
+    required int year,
+  }) {
+    final firstSnapshotInFirstMonth = snapshots.where((element) {
+      return element.dateTime.year == year;
+    }).first;
+
+    final int firstMonthInYear = firstSnapshotInFirstMonth.dateTime.month;
+
+    final lastSnapshotInLastMonth = snapshots.where((element) {
+      return element.dateTime.year == year;
+    }).last;
+
+    final int lastMonth = lastSnapshotInLastMonth.dateTime.month;
+
+    final List<int> monthsInYear = [
+      for (int i = firstMonthInYear; i <= lastMonth; i++) i
+    ];
+
+    final List<List<List<DateTime>>> items = [];
+
+    for (final month in monthsInYear) {
+      final List<List<DateTime>> item = UtilFunctions.weeksInMonth(
+        month,
+        year: year,
+        firstDayInMonth: firstSnapshotInFirstMonth.dateTime.day,
+        lastDayInMonth:
+            month == lastMonth ? lastSnapshotInLastMonth.dateTime.day : null,
+      );
+      items.add(item);
+    }
+    return items;
   }
 
   MapEntry<int, WeeklySnapshots> getWeeksPartForMonth({
@@ -144,7 +261,7 @@ class _CalendarViewerState extends State<CalendarViewer> {
 
     final Map<DateTime, ProductivitySnapshot> snapshotsInMonth = {};
     for (final day in daysInMonth) {
-      snapshotsInMonth[day] = balablu(day);
+      snapshotsInMonth[day] = snapshotFromDay(day);
     }
     final List<List<DateTime>> weeksInMonth = UtilFunctions.weeksInMonth(
       month,
